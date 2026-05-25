be3a1ea2c2fce7c81fc86c7916d4294d388517a7
anton0801 <danilovanton724@gmail.com>
2026-05-25 21:47:24 +0200

    Initial Commit

---
+++ Heat Build/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Heat Build
+//
+//  Created by Антон on 25/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Heat Build/Heat_BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heat_BuildApp.swift
+//  Heat Build
+//
+//  Created by Антон on 25/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Heat_BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
